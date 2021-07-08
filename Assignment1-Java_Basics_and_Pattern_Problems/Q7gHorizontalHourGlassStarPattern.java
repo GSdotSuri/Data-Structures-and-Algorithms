d7350fb1e1480c0b0d9339a6cd937a3f42b5aa04
GSdotSuri <gurprasadsinghofficial@gmail.com>
2021-07-08 16:10:08 +0530

    some mods

--- Assignment1-Java_Basics_and_Pattern_Problems/Q7gHorizontalHourGlassStarPattern.java
+++ Assignment1-Java_Basics_and_Pattern_Problems/Q7gHorizontalHourGlassStarPattern.java
@@ -43,37 +43,11 @@ public class Q7gHorizontalHourGlassStarPattern {
 
     }
 
-    public static void patternWithFor(int n){
-        int nst = 1, nsp = n-1;
-        for(int id = 1;id<=2*n-1;id++){
-
-            for(int csp = 1;csp <= nsp;csp++){
-                System.out.print(" ");
-            }
-
-            for(int cst = 1; cst<=nst;cst++){
-                 System.out.print("*");
-            }
-            System.out.println();
-            if(id<n){
-                nst = nst+2;
-                nsp = nsp - 1;
-            }
-            else{
-                nst = nst-2;
-                nsp = nsp+1;
-            }
-
-        }
-    }
     public static void main(String[] args) {
         System.out.println("Enter a number");
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         patternWithWhile(n);
-        System.out.println();
-        System.out.println();
-        //patternWithFor(n);
     }
     
 }
