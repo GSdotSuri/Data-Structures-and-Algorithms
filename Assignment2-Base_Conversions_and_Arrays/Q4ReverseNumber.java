eff6e458f5bbef268fb1e8cdb31aa5f5fc2aee31
GSdotSuri <gurprasadsinghofficial@gmail.com>
2021-07-10 16:12:48 +0530

    Q3

---
+++ Assignment2-Base_Conversions_and_Arrays/Q4ReverseNumber.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Q4ReverseNumber {
+
+    public static int reverseDigits(int n) {
+        int ans = 0, temp = 0;
+
+        while (n != 0) {
+            ans = ans * 10 + n % 10;
+            if (temp != (ans - (n % 2)) / 10) {
+                System.out.println("Reversed number went out of the range of int");
+                return 0;
+            }
+            temp = ans;
+            n = n / 10;
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter a number");
+        Scanner scn = new Scanner(System.in);
+        int number = scn.nextInt();
+        int reverse_number = reverseDigits(number);
+        System.out.print(reverse_number);
+
+    }
+
+}
