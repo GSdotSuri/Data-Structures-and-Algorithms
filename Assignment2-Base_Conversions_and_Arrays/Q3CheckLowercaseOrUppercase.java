0b2a8fb6cb599fa492b859cf6a2fd981d80cb02f
GSdotSuri <gurprasadsinghofficial@gmail.com>
2021-07-10 15:42:11 +0530

    Q3

---
+++ Assignment2-Base_Conversions_and_Arrays/Q3CheckLowercaseOrUppercase.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Q3CheckLowercaseOrUppercase {
+
+    public static String checkCase(char ch) {
+        String case_type;
+
+        if (ch >= 'A' && ch <= 'Z') {
+            case_type = "Uppercase";
+        } else if (ch >= 'a' && ch <= 'z') {
+            case_type = "Lowercase";
+        } else {
+            case_type = "Invalid Input";
+        }
+
+        return case_type;
+
+    }
+
+    public static void main(String[] args) {
+        char ch;
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter a character");
+        ch = scn.next().charAt(0);
+        System.out.println(checkCase(ch));
+
+    }
+
+}
