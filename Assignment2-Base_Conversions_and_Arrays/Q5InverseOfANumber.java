f29e763714fb2bb63ca767c022d00ed6f709c803
GSdotSuri <gurprasadsinghofficial@gmail.com>
2021-07-11 17:14:59 +0530

    Q5

---
+++ Assignment2-Base_Conversions_and_Arrays/Q5InverseOfANumber.java
@@ -0,0 +1,78 @@
+import java.util.*;
+
+public class Q5InverseOfANumber {
+    public static int hasDuplicates(int n) {
+        int count_arr[] = new int[10];
+        int count = 0, last_digit = 0;
+        while (n != 0) {
+            last_digit = n % 10;
+            count_arr[last_digit]++;
+            if (count_arr[last_digit] > 1) {
+                return 1;
+            }
+            n = n / 10;
+        }
+        return 0;
+    }
+
+    public static int countDigits(int n) {
+        int temp = n, ndigits = 0;
+        while (temp != 0) {
+            temp = temp / 10;
+            ndigits++;
+        }
+        return ndigits;
+
+    }
+
+    public static int validate(int n) {
+        int temp = n, last_digit;
+        int ndigits = countDigits(n);
+        while (temp != 0) {
+            last_digit = temp % 10;
+            temp = temp / 10;
+            if (last_digit < 1 || last_digit > ndigits) {
+                System.out.println(
+                        "The digit " + last_digit + " in the entered number doesen't lie in the range 1 to " + ndigits);
+                return 0;
+            } else {
+                continue;
+            }
+        }
+        if (hasDuplicates(n) == 1) {
+            System.out.println("The number" + n + "has duplicate characters");
+            return 0;
+        }
+        return 1;
+    }
+
+    public static int reverseNumber(int n) {
+        if (validate(n) == 1) {
+            System.out.println(1);
+            int power = 0, ans = 0, last_digit, ndigits = countDigits(n);
+            int temp = ndigits;
+            while (n != 0) {
+                last_digit = n % 10;
+                ans = ans + (int) Math.pow(10, ndigits - last_digit) * temp;
+                System.out.println(last_digit + " " + ans);
+                n = n / 10;
+                temp--;
+            }
+            return ans;
+        } else {
+            System.out.println(0);
+            System.out.println("Input is not validated");
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter a number to be reversed");
+        int n = scn.nextInt();
+        // System.out.println(validate(n));
+        // System.out.println(hasDuplicates(n));
+        System.out.println(reverseNumber(n));
+    }
+
+}
