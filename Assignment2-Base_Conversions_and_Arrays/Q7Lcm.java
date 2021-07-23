3b699d3ed8f021127d7092e0b91dde07f2490d0d
GSdotSuri <gurprasadsinghofficial@gmail.com>
2021-07-23 23:41:18 +0530

    Q7-lcm

---
+++ Assignment2-Base_Conversions_and_Arrays/Q7Lcm.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Q7Lcm {
+    public static int lcmTwoNoscm(int a, int b) {
+        int i, ans;
+        int max_num = Math.max(a, b);
+        for (i = max_num;;) {
+            if (i % a == 0 && i % b == 0) {
+                ans = i;
+                break;
+            }
+            i += max_num;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter first number");
+        int num1 = scn.nextInt();
+        System.out.println("Enter second number");
+        int num2 = scn.nextInt();
+        System.out.println((lcmTwoNoscm(num1, num2)));
+    }
+}
