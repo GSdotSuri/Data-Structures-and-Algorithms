cd7c8d8a02d13e1ff33e5ca18a65523731996439
GSdotSuri <gurprasadsinghofficial@gmail.com>
2021-07-24 00:44:39 +0530

    Q8-count multiples of in an array

---
+++ Assignment2-Base_Conversions_and_Arrays/Q8CountMultipleOf5.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Q8CountMultipleOf5 {
+
+    public static int countMultiples(int arr[]) {
+        int i, count = 0;
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i] % 5 == 0) {
+                count += 1;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no. of elements to store in the array: ");
+        int noe = sc.nextInt();
+        int[] arr = new int[noe];
+        for (int arr_index = 0; arr_index < noe; arr_index++) {
+            System.out.println(String.format("Enter the %dth value in the array", arr_index + 1));
+            arr[arr_index] = sc.nextInt();
+        }
+        int countofmultiples = countMultiples(arr);
+        System.out.println(countofmultiples);
+    }
+
+}
