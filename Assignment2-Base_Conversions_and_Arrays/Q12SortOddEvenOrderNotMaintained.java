45e44ff36f30ca92dd76000bb6e685b04f7100d7
GSdotSuri <gurprasadsinghofficial@gmail.com>
2021-07-25 19:01:43 +0530

    Q12- Sort odd and even numbers- not able to maintain the rder

---
+++ Assignment2-Base_Conversions_and_Arrays/Q12SortOddEvenOrderNotMaintained.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Q12SortOddEvenOrderNotMaintained {
+    public static void displayArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static int[] sortArray(int arr[]) {
+        int temp, low = 0, high = arr.length - 1;
+        while (high >= low) {
+            if (arr[low] % 2 != 0) {
+                if (arr[high] % 2 == 0) {
+                    temp = arr[low];
+                    arr[low] = arr[high];
+                    arr[high] = temp;
+                    low++;
+                    high--;
+                } else {
+                    high--;
+                }
+            } else {
+                low++;
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no. of elements to store in the array: ");
+        int noe = sc.nextInt();
+        int[] arr = new int[noe];
+        for (int arr_index = 0; arr_index < noe; arr_index++) {
+            System.out.println(String.format("Enter the %dth value in the array", arr_index + 1));
+            arr[arr_index] = sc.nextInt();
+        }
+        System.out.println("Original Array");
+        displayArray(arr);
+        System.out.println();
+        System.out.println("Sorted Array");
+        displayArray(sortArray(arr));
+    }
+}
